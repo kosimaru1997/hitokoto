00eadc25546d94d7c3f636d1e3bd92df3a58fcba
kosimaru1997 <kosimaru1997@gmail.com>
2021-07-29 11:48:12 +0900

    post function

--- app/controllers/hitokoto_controller.rb
+++ app/controllers/hitokoto_controller.rb
@@ -3,9 +3,27 @@ class HitokotoController < ApplicationController
     @post = Post.new
   end
 
+  def create
+    post = Post.new(post_params)
+    sentiment = post.get_sentiment
+    post.score = sentiment[:score]
+    post.magnitude = sentiment[:magnitude]
+    post.random_int1 = rand(rand(200))
+    post.random_int2 = rand(rand(200))
+    if post.save
+      redirect_to hitokoto_path(post)
+    else
+      redirect_to new
+    end
+  end
+
   def index
   end
 
   def show
+  end
+
+  def post_params
+    params.require(:post).permit(:post)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,36 @@
 class Post < ApplicationRecord
+
+  def get_sentiment
+    require 'net/http'
+    require 'uri'
+    require 'json'
+
+    text = self.post
+
+    uri = URI.parse("https://language.googleapis.com/v1beta1/documents:analyzeSentiment?key=#{ENV["GOOGlE_API_KYE"]}")
+    request = Net::HTTP::Post.new(uri)
+    byebug
+    request.content_type = "application/json"
+    request.body = ""
+    request.body = {
+                      document:{
+                        type:'PLAIN_TEXT',
+                        content: text
+                      }
+                    }.to_json
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    json = JSON.parse(response.body)
+    score =  json['documentSentiment']['score']*100
+    magnitude = json['documentSentiment']['magnitude']*100
+    sentiment = {score: score.to_i, magnitude: magnitude.to_i}
+  end
+
 end
